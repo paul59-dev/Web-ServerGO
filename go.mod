7b1516352364caf15ecd9721f659f88d3ad990e8
Paul-developpeur <paulallebee@gmail.com>
2024-01-12 19:15:07 +0100

    add router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module paul/webServer
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	//http.HandleFunc("/", homeHandler) // Response with all route
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Router
+	r := mux.NewRouter()
+	r.HandleFunc("/book/{title}/page/{page}", bookHandler)
+
+	http.Handle("/", r)
+
 	http.ListenAndServe(":80", nil)
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+// }
+
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var title string = vars["title"]
+	var page string = vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book %s on page %s\n", title, page)
 }
